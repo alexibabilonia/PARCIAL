d8ef292f471558a2d8ab55be494281ef1b02b7ce
alexibabilonia <babiloniaalexi@gmail.com>
2025-09-05 23:44:45 -0500

    Ahora si ultimo commit porque no me di cuneta de un error jeje

--- src/app/guards/is-logged-guard.spec.ts
+++ src/app/guards/is-logged-guard.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { IsLoggedGuard } from './is.logged-guard';
 
-import { isLoggedGuard } from './is.logged-guard';
-
-describe('isLoggedGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => isLoggedGuard(...guardParameters));
+describe('IsLoggedGuard', () => {
+  let guard: IsLoggedGuard;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule]
+    });
+
+    guard = TestBed.inject(IsLoggedGuard);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('should block access if user is logged in', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('{"email":"test@test.com"}');
+    const navigateSpy = spyOn(router, 'navigate');
+
+    const result = guard.canActivate();
+
+    expect(result).toBeFalse();
+    expect(navigateSpy).toHaveBeenCalledWith(['/home']);
+  });
+
+  it('should allow access if no user is logged in', () => {
+    spyOn(localStorage, 'getItem').and.returnValue(null);
+
+    const result = guard.canActivate();
+
+    expect(result).toBeTrue();
   });
 });
